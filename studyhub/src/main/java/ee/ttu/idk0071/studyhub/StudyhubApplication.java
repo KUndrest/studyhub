882fae10cb7bf7fa5a64039af10581f352dcbd68
KUndrest <kristinundrest@gmail.com>
2018-02-14 09:48:04 +0200

    Initial commit

---
+++ studyhub/src/main/java/ee/ttu/idk0071/studyhub/StudyhubApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.idk0071.studyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyhubApplication.class, args);
+	}
+}
