49326be8ce1dd46e90494e7dd3515d081aac1697
KUndrest <kristinundrest@gmail.com>
2018-03-02 12:42:30 +0200

    Revert "aurelia jama"
    This reverts commit 79614b5

--- aurelia/src/app.js
+++ aurelia/src/app.js
@@ -1,14 +1,14 @@
-import 'bootsrap';
+import 'bootstrap';
 
 export class App {
   configureRouter(config, router) {
     this.router = router;
     config.title = 'StudyHub';
     config.map([
-      { route: ['', 'home'],       name: 'home',       moduleId: 'home/home' },
+      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
       { route: 'users',            name: 'users',      moduleId: 'users/index', nav: true, title: 'Users' },
-      //{ route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' },
-      //{ route: 'files/*path',      name: 'files',      moduleId: 'files/index', nav: 0,    title: 'Files', href:'#files' }
+      { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' },
+      { route: 'files/*path',      name: 'files',      moduleId: 'files/index', nav: 0,    title: 'Files', href:'#files' }
     ]);
   }
 }
