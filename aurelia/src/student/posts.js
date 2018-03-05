787fee325250c737916ebb8ab2929e17927d8ace
KUndrest <kristinundrest@gmail.com>
2018-03-05 23:02:34 +0200

    Koduste ülesannete näitamise ja lisamise võimalused tudengile

--- aurelia/src/student/posts.js
+++ aurelia/src/student/posts.js
@@ -1,7 +1,16 @@
 import 'bootstrap';
+import {HttpClient} from 'aurelia-fetch-client';
 
 export class post {
-  constructor() {
+  postList = [];
 
+  constructor() {
+  }
+  activate() {
+    let client = new HttpClient();
+
+    client.fetch('http://localhost:8080/posts')
+      .then(response => response.json())
+      .then(posts => this.postList = posts);
   }
 }
--- aurelia/src/student/student.js
+++ aurelia/src/student/student.js
@@ -2,9 +2,22 @@ import {HttpClient} from 'aurelia-fetch-client';
 import 'bootstrap';
 
 export class student {
+  postData = {};
   subjectList = [];
 
   constructor() {
+  }
+  addPost() {
+    let client = new HttpClient();
+
+    client.fetch('http://localhost:8080/posts/add', {
+      'method': 'POST',
+      'body': json(this.postData)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Server saatis' + data.post);
+      });
   }
   activate() {
     let client = new HttpClient();
