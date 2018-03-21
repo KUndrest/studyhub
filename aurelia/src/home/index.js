84cf35834594dc1c832172c1bb3bbcf611df9cb6
KUndrest <kristinundrest@gmail.com>
2018-03-21 12:08:33 +0200

    pealehe vormi parandused + footer + hrefide muudatused

--- aurelia/src/home/index.js
+++ aurelia/src/home/index.js
@@ -37,23 +37,20 @@ export class home {
       document.getElementById('studentCode').style.display = 'none';
     }
   }
-
-
-  /*
-  $(function() {
-      $('#login-form-link').click(function(e) {
-          $("#login-form").delay(100).fadeIn(100);
-          $("#register-form").fadeOut(100);
-          $('#register-form-link').removeClass('active');
-          $(this).addClass('active');
-          e.preventDefault();
-      });
-      $('#register-form-link').click(function(e) {
-          $("#register-form").delay(100).fadeIn(100);
-          $("#login-form").fadeOut(100);
-          $('#login-form-link').removeClass('active');
-          $(this).addClass('active');
-          e.preventDefault();
-      });
-  }); */
+  changeForm() {
+    $('#login-form-link').click(function(e) {
+      $('#login-form').delay(100).fadeIn(100);
+      $('#register-form').fadeOut(100);
+      $('#register-form-link').removeClass('active');
+      $(this).addClass('active');
+      e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+      $('#register-form').delay(100).fadeIn(100);
+      $('#login-form').fadeOut(100);
+      $('#login-form-link').removeClass('active');
+      $(this).addClass('active');
+      e.preventDefault();
+    });
+  }
 }
